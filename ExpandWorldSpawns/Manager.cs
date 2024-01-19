e59ee6d375ffa9be233f1a2abef48f4e307b6fe1
Jere Kuusela <jere.kuusela@iki.fi>
2024-01-19 16:43:09 +0200

    Some todo

--- ExpandWorldSpawns/Manager.cs
+++ ExpandWorldSpawns/Manager.cs
@@ -12,7 +12,6 @@ public class Manager
   public static string FileName = "expand_spawns.yaml";
   public static string FilePath = Path.Combine(EWD.YamlDirectory, FileName);
   public static string Pattern = "expand_spawns*.yaml";
-  public static List<SpawnSystem.SpawnData> Originals = [];
 
   public static bool IsValid(SpawnSystem.SpawnData spawn) => spawn.m_prefab;
   public static string Save()
@@ -27,11 +26,6 @@ public class Manager
   public static void ToFile()
   {
     if (Helper.IsClient()) return;
-    if (Originals.Count == 0)
-    {
-      var spawnSystem = SpawnSystem.m_instances.FirstOrDefault();
-      Originals = spawnSystem.m_spawnLists.SelectMany(s => s.m_spawners).ToList();
-    }
     if (File.Exists(FilePath)) return;
     var yaml = Save();
     Configuration.valueSpawnData.Value = yaml;
@@ -45,9 +39,11 @@ public class Manager
   }
   public static void FromSetting(string yaml)
   {
+    // First load is delayed because RRR uses ServerSync to create monsters (race condition).
+    if (HandleSpawnData.Override == null) return;
     if (Helper.IsClient()) Set(yaml);
   }
-  private static void Set(string yaml)
+  public static void Set(string yaml)
   {
     HandleSpawnData.Override = null;
 
@@ -63,11 +59,6 @@ public class Manager
         EWS.LogWarning($"Failed to load any spawn data.");
         return;
       }
-      if (ExpandWorldData.Configuration.DataMigration && Helper.IsServer() && AddMissingEntries(data))
-      {
-        // Watcher triggers reload.
-        return;
-      }
       EWS.LogInfo($"Reloading spawn data ({data.Count} entries).");
       HandleSpawnData.Override = data;
       SpawnSystem.m_instances.ForEach(HandleSpawnData.Set);
@@ -79,23 +70,6 @@ public class Manager
     }
   }
 
-  private static bool AddMissingEntries(List<SpawnSystem.SpawnData> entries)
-  {
-    var missingKeys = Originals.Select(s => s.m_prefab.name).Distinct().ToHashSet();
-    foreach (var item in entries)
-      missingKeys.Remove(item.m_prefab.name);
-    if (missingKeys.Count == 0) return false;
-    var missing = Originals.Where(item => missingKeys.Contains(item.m_prefab.name)).ToList();
-    EWS.LogWarning($"Adding {missing.Count} missing spawns to the expand_spawns.yaml file.");
-    foreach (var item in missing)
-      EWS.LogWarning(item.m_prefab.name);
-    var yaml = File.ReadAllText(FilePath);
-    var data = DataManager.Serializer().Serialize(missing.Select(Loader.ToData));
-    // Directly appending is risky but necessary to keep comments, etc.
-    yaml += "\n" + data;
-    File.WriteAllText(FilePath, yaml);
-    return true;
-  }
   public static void SetupWatcher()
   {
     DataManager.SetupWatcher(Pattern, FromFile);
@@ -109,10 +83,10 @@ public class InitializeContent
 {
   static void Postfix()
   {
+    HandleSpawnData.Override = null;
     if (Helper.IsServer())
     {
       Manager.FromFile();
-      // Spawn data is handled elsewhere.
     }
   }
 }
@@ -120,26 +94,26 @@ public class InitializeContent
 [HarmonyPatch(typeof(SpawnSystem), nameof(SpawnSystem.Awake))]
 public class HandleSpawnData
 {
-  // File exist check might be bit too slow for constant checking.
-  static bool Done = false;
   public static List<SpawnSystem.SpawnData>? Override = null;
   static void Postfix(SpawnSystem __instance)
   {
-    if (ZNet.instance.IsServer() && !Done)
+    if (Override == null)
     {
-      Manager.ToFile();
-      Done = true;
+      if (Helper.IsClient() && Configuration.valueSpawnData.Value != "")
+        Manager.Set(Configuration.valueSpawnData.Value);
+      if (Helper.IsServer())
+        Manager.ToFile();
     }
     Set(__instance);
   }
 
   public static void Set(SpawnSystem system)
   {
-    if (Override != null)
-    {
-      while (system.m_spawnLists.Count > 1)
-        system.m_spawnLists.RemoveAt(system.m_spawnLists.Count - 1);
-      system.m_spawnLists[0].m_spawners = Override;
-    }
+    if (Override == null) return;
+
+    while (system.m_spawnLists.Count > 1)
+      system.m_spawnLists.RemoveAt(system.m_spawnLists.Count - 1);
+    system.m_spawnLists[0].m_spawners = Override;
+
   }
 }
